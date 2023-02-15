3783b4a9805711e5115a2287ed305b9c4e13d0e5
Techno136 <mustafakeskin136@hotmail.com>
2023-02-15 15:37:56 +0300

    yeni

--- app/src/main/java/com/example/proje_1/caradapter.java
+++ app/src/main/java/com/example/proje_1/caradapter.java
@@ -2,7 +2,7 @@ package com.example.proje_1;
 
 public class caradapter {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Car car1= new Car();
 
